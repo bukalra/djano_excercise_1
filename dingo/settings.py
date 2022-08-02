0fcf181f39c26660a18d74c13514d7146b14d818
Radek Bukalski <bukalskiradek@gmail.com>
2022-08-02 19:50:21 +0200

    REST and depl5

--- dingo/settings.py
+++ dingo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', "key testowy")
+SECRET_KEY = SECRET_KEY ='django-insecure-2qc+_7^d(5rq)lc4kgh)gy7mykllm7xt3t_(1ca^$8a26&djc+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
